bcfa3f4a5c8da61fcc8dc1c89e292a14491fc284
ds12a <81164854+ds12a@users.noreply.github.com>
2023-08-11 13:12:54 -0400

    Toggle pose visualization

--- WebInterface/walleye/src/Components/PoseVisualizationList.jsx
+++ WebInterface/walleye/src/Components/PoseVisualizationList.jsx
@@ -1,4 +1,4 @@
-import { Form, Image } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import PoseVisualization from "./PoseVisualization";
 import { socket } from "../socket";
 import { useState } from "react";
@@ -6,17 +6,17 @@ import { useState } from "react";
 export default function PoseVisualizationList(props) {
     const [isRendering, setIsRendering] = useState(props.state.visualizingPoses);
 
-    function onToggle() {
+    const onToggle = () => {
         setIsRendering(!isRendering);
         socket.emit("toggle_pose_visualization", isRendering);
-    }
+    };
 
     return (
         <>
             <Form>
                 <Form.Check
                     type="switch"
-                    onChange={() => onToggle()}
+                    onChange={onToggle}
                     label="Pose Visualization Toggle"
                     checked={isRendering}
                 />
