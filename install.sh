edadb0e55333cc149a68c5d06b19e36ec2f3f3db
ds12a <81164854+ds12a@users.noreply.github.com>
2024-10-17 18:38:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 set -e
 echo "Beginning wallEYE Installation..."
 
+
+if [ "$EUID" -ne 0 ]
+  then echo "Run as root!"
+  exit
+fi
+
 USER="$(whoami)"
 
 # Python dependencies
@@ -8,22 +14,22 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.12 python3.12-venv
 
-cd ~
-
-git clone https://github.com/strykeforce/wallEYE.git -b Refactoring --single-branch
-
-cd wallEYE/PiSideCode
-
-python3.12 -m venv env
-
-source env/bin/activate
-
 wget https://bootstrap.pypa.io/get-pip.py
 python3.12 get-pip.py
 
 rm get-pip.py
 
-python3.12 -m pip install --upgrade setuptools
+python3.12 -m venv env
+
+source env/bin/activate
+
+# Download wallEYE
+cd ~
+
+git clone https://github.com/strykeforce/wallEYE.git
+
+cd wallEYE/PiSideCode
+
 python3.12 -m pip install -r requirements.txt
 
 deactivate
@@ -34,19 +40,15 @@ sudo apt install v4l-utils net-tools openssh-server
 sudo chmod 4755 /usr/sbin/ifconfig
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-echo "Node.js and npm version: $(node -v) $(npm -v)"
-
 nvm install 20
+
+echo "Node.js and npm version: $(node -v) $(npm -v)"
 
 cd web_interface/walleye
 npm install
 
 # Service file
-sudo echo "[Unit]
+echo "[Unit]
 Description=WallEYE Vision System Service
 After=default.target
 StartLimitIntervalSec=0
@@ -54,16 +56,14 @@ StartLimitIntervalSec=0
 Type=simple
 Restart=always
 RestartSec=1
-WorkingDirectory=/home/$(USER)/wallEYE/PiSideCode
-User=$(USER)
-ExecStart=+/home/strykeforce/wallEYE/PiSideCode/env/bin/python3.12 /home/$(USER)/wallEYE/PiSideCode/init.py
+WorkingDirectory=/home/${USER}/wallEYE/PiSideCode
+User=${USER}
+ExecStart=+/home/strykeforce/wallEYE/PiSideCode/env/bin/python3.12 /home/${USER}/wallEYE/PiSideCode/init.py
 
 [Install]
 WantedBy=default.target" > /etc/systemd/system/walleye.service
 
 sudo systemctl enable walleye
 sudo systemctl start walleye
-
-chmod -R 777 "/home/$(USER)/wallEYE"
 
 echo "wallEYE Installation Complete!"
