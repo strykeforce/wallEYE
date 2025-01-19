35748dee0270ec42ea1ac786080b6acd5de05c08
ds12a <dshen1248@gmail.com>
2025-01-19 14:52:56 -0500

    version 3.13

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ echo "Installing as ${user}"
 # Python dependencies
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
-sudo apt-get install python3.12 python3.12-venv python3.12-dev
+sudo apt-get upgrade
+sudo apt-get install python3.13 python3.13-venv python3.13-dev
 
 cd ~
 
@@ -16,17 +17,17 @@ git clone https://github.com/strykeforce/wallEYE.git -b Refactoring --single-bra
 
 cd wallEYE/PiSideCode
 
-python3.12 -m venv env
+python3.13 -m venv env
 
 source env/bin/activate
 
 wget https://bootstrap.pypa.io/get-pip.py
-python3.12 get-pip.py
+python3.13 get-pip.py
 
 rm get-pip.py
 
-python3.12 -m pip install --upgrade setuptools
-python3.12 -m pip install -r requirements.txt
+python3.13 -m pip install --upgrade setuptools
+python3.13 -m pip install -r requirements.txt
 
 deactivate
 
@@ -63,7 +64,7 @@ WorkingDirectory=/home/${user}/wallEYE/PiSideCode
 User=${user}
 ExecStartPre=+/sbin/rmmod uvcvideo
 ExecStartPre=+/sbin/modprobe uvcvideo nodrop=1 timeout=5000 quirks=0x80
-ExecStart=+/home/${user}/wallEYE/PiSideCode/env/bin/python3.12 /home/${user}/wallEYE/PiSideCode/init.py
+ExecStart=+/home/${user}/wallEYE/PiSideCode/env/bin/python3.13 /home/${user}/wallEYE/PiSideCode/init.py
 
 [Install]
 WantedBy=default.target\" > /etc/systemd/system/walleye.service"
